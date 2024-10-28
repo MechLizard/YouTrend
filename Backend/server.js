7d87624343aa174e8ff17b8090bb993c52aea84f
adalysg <adalys39@gmail.com>
2024-10-28 14:25:27 -0400

    POST user request working

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const oracledb = require('oracledb');
+const bodyParser = require('body-parser');
 require('dotenv').config({ path: '../.env' });
 
 const app = express();
 const PORT = 5000;
+
+app.use(bodyParser.json());
 
 async function connectToDatabase() {
     try {
@@ -22,8 +25,8 @@ app.get('/', (req, res) => {
     res.send("API is working!");
 });
 
-// GET Data from "User" table in DB
-app.get('/api/users', async (req, res) => {
+// GET ALL Data from "User" table in DB
+app.get('/api/users/get', async (req, res) => {
     let connection;
     try {
         connection = await oracledb.getConnection();
@@ -33,6 +36,46 @@ app.get('/api/users', async (req, res) => {
     } catch (err) {
         console.error(err);
         res.status(500).send("Error fetching data");
+    } finally {
+        if (connection) {
+            await connection.close();
+        }
+    }
+});
+
+// To-Do: GET specific user's info
+
+// POST (send) Data to "User" table in DB
+app.post('/api/users/send', async (req, res) => {
+    if (!req.body) {
+        return res.status(400).send("Request body is missing");
+    }
+    
+    const { user_name, password, country_name } = req.body; // add queries
+    let connection;
+
+    try {
+        connection = await oracledb.getConnection();
+
+         // handles queries of type blob
+        // const queriesBlob = queries ? Buffer.from(queries, 'base64') : null;
+
+        const sql_insert = `INSERT INTO "User" (user_name, "password", country_name) VALUES (:user_name, :password, :country_name)`; // add queriesBlob
+
+        await connection.execute(sql_insert, {
+            user_name: user_name,
+            password: password,
+            // queries: queriesBlob,
+            country_name: country_name
+        });
+
+        await connection.commit();
+
+        res.status(201).send("User created successfully");
+
+    } catch (err) {
+        console.error("Error inserting data:", err.message);
+        res.status(500).send("Error inserting data");
     } finally {
         if (connection) {
             await connection.close();
