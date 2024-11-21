83dbbf8fc101f5c2a9ade63d506f3feae8f45ea2
adalysg <adalys39@gmail.com>
2024-11-21 10:08:08 -0500

    Added lines after queries to close connection, fixes timeout bug

--- Backend/dbQueries.js
+++ Backend/dbQueries.js
@@ -4,55 +4,54 @@ const oracledb = require('oracledb');
 async function fetchTimeDaySuccess({ country, categoryId, startDate, endDate, tag }) {
     let connection;
     try {
-        connection = await oracledb.getConnection();
-        let time_day_sql = `
-            SELECT 
-                unique_video.publish_date, 
-                unique_video.published_day_of_week, 
-                SUM(unique_video.views) AS views,
-                SUM(unique_video.likes) AS likes, 
-                SUM(unique_video.dislikes) AS dislikes, 
-                SUM(unique_video.comment_count) AS comment_count,
-                MAX(best_daily.top_video_id) AS top_video_id,
-                MAX(best_daily.top_video_title) AS top_video_title,
-                MAX(best_daily.top_video_views) AS top_video_views,
-                MAX(best_daily.top_video_likes) AS top_video_likes,
-                MAX(best_daily.top_video_dislikes) AS top_video_dislikes
+      connection = await oracledb.getConnection();
+      let time_day_sql = `
+        SELECT 
+            unique_video.publish_date, 
+            unique_video.published_day_of_week, 
+            SUM(unique_video.views) AS views,
+            SUM(unique_video.likes) AS likes, 
+            SUM(unique_video.dislikes) AS dislikes, 
+            SUM(unique_video.comment_count) AS comment_count,
+            MAX(best_daily.top_video_id) AS top_video_id,
+            MAX(best_daily.top_video_title) AS top_video_title,
+            MAX(best_daily.top_video_views) AS top_video_views,
+            MAX(best_daily.top_video_likes) AS top_video_likes,
+            MAX(best_daily.top_video_dislikes) AS top_video_dislikes
+        FROM (
+            SELECT publish_date, published_day_of_week, views, likes, dislikes, comment_count,
+                    ROW_NUMBER() OVER (PARTITION BY video.video_id ORDER BY views DESC) AS rn
+            FROM Video
+            ${tag ? 'JOIN tag_association ON video.video_id = tag_association.video_id' : ''} 
+            WHERE (publish_country LIKE :country OR :country = '%')
+              AND (category_id = :category_id OR :category_id IS NULL)
+              AND publish_date BETWEEN TO_DATE(:start_date, 'DD-MM-YY') AND TO_DATE(:end_date, 'DD-MM-YY')
+              ${tag ? 'AND tag_association.tag_string LIKE :tag' : ''} 
+        ) unique_video
+        JOIN (
+            SELECT publish_date AS top_video_publish_date, 
+                    video_id AS top_video_id,
+                    title AS top_video_title,
+                    views AS top_video_views,
+                    likes AS top_video_likes,
+                    dislikes AS top_video_dislikes
             FROM (
-                SELECT publish_date, published_day_of_week, views, likes, dislikes, comment_count,
-                       ROW_NUMBER() OVER (PARTITION BY video.video_id ORDER BY views DESC) AS rn
+                SELECT publish_date, video.video_id, title, views, likes, dislikes,
+                        ROW_NUMBER() OVER (PARTITION BY publish_date ORDER BY views DESC) AS day_rank
                 FROM Video
                 ${tag ? 'JOIN tag_association ON video.video_id = tag_association.video_id' : ''} 
                 WHERE (publish_country LIKE :country OR :country = '%')
                   AND (category_id = :category_id OR :category_id IS NULL)
                   AND publish_date BETWEEN TO_DATE(:start_date, 'DD-MM-YY') AND TO_DATE(:end_date, 'DD-MM-YY')
                   ${tag ? 'AND tag_association.tag_string LIKE :tag' : ''} 
-            ) unique_video
-            JOIN (
-                SELECT publish_date AS top_video_publish_date, 
-                       video_id AS top_video_id,
-                       title AS top_video_title,
-                       views AS top_video_views,
-                       likes AS top_video_likes,
-                       dislikes AS top_video_dislikes
-                FROM (
-                    SELECT publish_date, video.video_id, title, views, likes, dislikes,
-                           ROW_NUMBER() OVER (PARTITION BY publish_date ORDER BY views DESC) AS day_rank
-                    FROM Video
-                    ${tag ? 'JOIN tag_association ON video.video_id = tag_association.video_id' : ''} 
-                    WHERE (publish_country LIKE :country OR :country = '%')
-                      AND (category_id = :category_id OR :category_id IS NULL)
-                      AND publish_date BETWEEN TO_DATE(:start_date, 'DD-MM-YY') AND TO_DATE(:end_date, 'DD-MM-YY')
-                      ${tag ? 'AND tag_association.tag_string LIKE :tag' : ''} 
-                )
-                WHERE day_rank = 1
-            ) best_daily 
-            ON unique_video.publish_date = top_video_publish_date
-            WHERE rn = 1
-            GROUP BY unique_video.publish_date, unique_video.published_day_of_week
-            ORDER BY unique_video.publish_date
-        `;
-
+            )
+            WHERE day_rank = 1
+        ) best_daily 
+        ON unique_video.publish_date = top_video_publish_date
+        WHERE rn = 1
+        GROUP BY unique_video.publish_date, unique_video.published_day_of_week
+        ORDER BY unique_video.publish_date
+      `;
 
       const attributes = {
         country: country || '%',
@@ -71,6 +70,10 @@ async function fetchTimeDaySuccess({ country, categoryId, startDate, endDate, ta
     } catch (err) {
         console.error("Error executing 'Time/Day Success' query:", err);
         throw err;
+    } finally {
+      if (connection) {
+        await connection.close(); // closes connection to prevent timeouts
+      }
     }
 };
 
@@ -177,17 +180,19 @@ async function fetchDisabledVideos({
       attributes.tag = `%${tag}%`;
     }
 
-    console.log("Inputs: ", attributes);
-
     const result = await connection.execute(disabled_sql, attributes);
-
     return result.rows;
 
   } catch (err) {
     console.error("Error executing 'Disabled Videos' query:", err);
     throw err;
+  } finally {
+    if (connection) {
+      await connection.close(); // closes connection to prevent timeouts
+    }
   }
 }
+
 
 // *=== Events ===* //
 async function fetchTrendingData({ country, categoryId, startDate, endDate, tag }) {
@@ -264,8 +269,13 @@ async function fetchTrendingData({ country, categoryId, startDate, endDate, tag 
   } catch (err) {
     console.error("Error executing 'Trending Data' query:", err);
     throw err;
+  } finally {
+    if (connection) {
+      await connection.close(); // closes connection to prevent timeouts
+    }
   }
 }
+
 
 async function fetchEventInfo(eventName) {
   let connection;
@@ -284,8 +294,13 @@ async function fetchEventInfo(eventName) {
   } catch (err) {
     console.error("Error executing 'Event Info' query:", err);
     throw err;
+  } finally {
+    if (connection) {
+      await connection.close(); // closes connection to prevent timeouts
+    }
   }
 }
+
 
 // *=== Popularity ===* //
 async function fetchPopularityData({ country, categoryId, startDate, endDate, tag }) {
@@ -363,8 +378,13 @@ async function fetchPopularityData({ country, categoryId, startDate, endDate, ta
   } catch (err) {
     console.error("Error executing 'fetchPopularityData' query:", err);
     throw err;
+  } finally {
+    if (connection) {
+      await connection.close(); // closes connection to prevent timeouts
+    }
   }
 }
+
 
 // *=== Sentiment ===* //
 async function fetchSentimentData({ country, categoryId, startDate, endDate, tag }) {
@@ -442,6 +462,10 @@ async function fetchSentimentData({ country, categoryId, startDate, endDate, tag
   } catch (err) {
     console.error("Error executing fetchSentimentData query:", err);
     throw err;
+  } finally {
+    if (connection) {
+      await connection.close(); // closes connection to prevent timeouts
+    }
   }
 }
 
